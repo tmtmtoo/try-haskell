8053c392e4797f5856586b7de7a1a6be3a950f7c
tmtmtoo <tmtmt2209@gmail.com>
2026-04-25 09:41:24 +0000

    chore: lesson 001 basic

--- 001-basics/src/Lesson.hs
+++ 001-basics/src/Lesson.hs
@@ -1,26 +1,40 @@
 module Lesson
-  ( square
-  , factorial
-  , fizzbuzz
-  , myLength
-  , myMap
-  , pythagoreanTriples
-  ) where
+  ( square,
+    factorial,
+    fizzbuzz,
+    myLength,
+    myMap,
+    pythagoreanTriples,
+  )
+where
 
 square :: Int -> Int
-square = undefined
+square n = n * n
 
 factorial :: Integer -> Integer
-factorial = undefined
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
 
 fizzbuzz :: Int -> String
-fizzbuzz = undefined
+fizzbuzz n
+  | n `mod` 15 == 0 = "FizzBuzz"
+  | n `mod` 3 == 0 = "Fizz"
+  | n `mod` 5 == 0 = "Buzz"
+  | otherwise = show n
 
 myLength :: [a] -> Int
-myLength = undefined
+myLength [] = 0
+myLength (_ : xs) = 1 + myLength xs
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap = undefined
+myMap _ [] = []
+myMap f (x : xs) = [f x] ++ myMap f xs
 
 pythagoreanTriples :: Int -> [(Int, Int, Int)]
-pythagoreanTriples = undefined
+pythagoreanTriples n =
+  [ (a, b, c)
+  | a <- [1 .. n],
+    b <- [1 .. n],
+    c <- [1 .. n],
+    a * a + b * b == c * c && a <= b && b <= c
+  ]
