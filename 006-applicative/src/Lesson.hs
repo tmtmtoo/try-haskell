8bf7efa6d04c5ec5a271d4572a4997c91ae735ed
tmtmtoo <tmtmt2209@gmail.com>
2026-04-29 05:44:44 +0000

    chore: lesson 006 applicative

--- 006-applicative/src/Lesson.hs
+++ 006-applicative/src/Lesson.hs
@@ -1,32 +1,42 @@
 module Lesson
-  ( Validation (..)
-  , Person (..)
-  , validateName
-  , validateAge
-  , mkPerson
-  ) where
+  ( Validation (..),
+    Person (..),
+    validateName,
+    validateAge,
+    mkPerson,
+  )
+where
 
 data Validation e a = Failure e | Success a
   deriving (Eq, Show)
 
 instance Functor (Validation e) where
-  fmap = undefined
+  fmap _ (Failure e) = Failure e
+  fmap f (Success a) = Success $ f a
 
-instance Semigroup e => Applicative (Validation e) where
-  pure  = undefined
-  (<*>) = undefined
+instance (Semigroup e) => Applicative (Validation e) where
+  pure = Success
+  Failure x <*> Failure y = Failure $ x <> y
+  Failure x <*> Success _ = Failure x
+  Success _ <*> Failure x = Failure x
+  Success f <*> Success x = Success $ f x
 
 data Person = Person
-  { personName :: String
-  , personAge  :: Int
+  { personName :: String,
+    personAge :: Int
   }
   deriving (Eq, Show)
 
 validateName :: String -> Validation [String] String
-validateName = undefined
+validateName name
+  | name == "" = Failure ["name is empty"]
+  | otherwise = Success name
 
 validateAge :: Int -> Validation [String] Int
-validateAge = undefined
+validateAge age
+  | age < 0 = Failure ["age out of range"]
+  | age >= 150 = Failure ["age out of range"]
+  | otherwise = Success age
 
 mkPerson :: String -> Int -> Validation [String] Person
-mkPerson = undefined
+mkPerson name age = Person <$> validateName name <*> validateAge age
