dc4bf904fea3841ae865f38c0aef5d1749a79caa
tmtmtoo <tmtmt2209@gmail.com>
2026-04-29 05:21:30 +0000

    chore: lesson 005 functor

--- 005-functor/src/Lesson.hs
+++ 005-functor/src/Lesson.hs
@@ -9,16 +9,17 @@ data Tree a = Leaf | Node (Tree a) a (Tree a)
   deriving (Eq, Show)
 
 instance Functor Tree where
-  fmap = undefined
+  fmap _ Leaf = Leaf
+  fmap f (Node l x r) = Node (f <$> l) (f x) (f <$> r)
 
 newtype Pair a = Pair (a, a)
   deriving (Eq, Show)
 
 instance Functor Pair where
-  fmap = undefined
+  fmap f (Pair (x, y)) = Pair (f x, f y)
 
 incrAll :: Functor f => f Int -> f Int
-incrAll = undefined
+incrAll = fmap (+1)
 
 replaceAll :: Functor f => b -> f a -> f b
-replaceAll = undefined
+replaceAll x y = x <$ y
