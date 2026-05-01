3adc66611187096b9ff27d8dbc25c6fa959a97a4
tmtmtoo <tmtmt2209@gmail.com>
2026-05-01 04:51:55 +0000

    chore: lesson 008 foldable traversable monoid

--- 008-foldable-traversable-monoid/src/Lesson.hs
+++ 008-foldable-traversable-monoid/src/Lesson.hs
@@ -1,32 +1,36 @@
 module Lesson
-  ( Min' (..)
-  , Tree (..)
-  , parseInts
-  , treeSum
-  ) where
+  ( Min' (..),
+    Tree (..),
+    parseInts,
+    treeSum,
+  )
+where
 
 import Text.Read (readMaybe)
 
-newtype Min' a = Min' { getMin' :: a }
+newtype Min' a = Min' {getMin' :: a}
   deriving (Eq, Show)
 
-instance Ord a => Semigroup (Min' a) where
-  (<>) = undefined
+instance (Ord a) => Semigroup (Min' a) where
+  Min' x <> Min' y = Min' $ min x y
 
 data Tree a = Leaf | Node (Tree a) a (Tree a)
   deriving (Eq, Show)
 
 instance Functor Tree where
-  fmap = undefined
+  fmap _ Leaf = Leaf
+  fmap f (Node l x r) = Node (f <$> l) (f x) (f <$> r)
 
 instance Foldable Tree where
-  foldr = undefined
+  foldr _ z Leaf = z
+  foldr f z (Node l x r) = foldr f (f x $ foldr f z r) l
 
 instance Traversable Tree where
-  traverse = undefined
+  traverse _ Leaf = pure Leaf
+  traverse f (Node l x r) = Node <$> traverse f l <*> f x <*> traverse f r
 
 parseInts :: [String] -> Maybe [Int]
 parseInts = traverse readMaybe
 
-treeSum :: Num a => Tree a -> a
-treeSum = undefined
+treeSum :: (Num a) => Tree a -> a
+treeSum = sum
