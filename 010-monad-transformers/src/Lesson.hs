b87aa48d690c8e2b0b310e73b2a57b189a0b3853
tmtmtoo <tmtmt2209@gmail.com>
2026-05-01 07:16:54 +0000

    chore: lesson 010 monad transformers

--- 010-monad-transformers/src/Lesson.hs
+++ 010-monad-transformers/src/Lesson.hs
@@ -1,35 +1,51 @@
 module Lesson
-  ( Inventory
-  , decreaseStock
-  , runStock
-  , AppConfig (..)
-  , decorate
-  , runDecorate
-  ) where
+  ( Inventory,
+    decreaseStock,
+    runStock,
+    AppConfig (..),
+    decorate,
+    runDecorate,
+  )
+where
 
-import Control.Monad.Except (ExceptT)
-import Control.Monad.Reader (ReaderT)
-import Control.Monad.State (State)
+import Control.Monad.Except (ExceptT, MonadError (throwError), runExceptT)
+import Control.Monad.Reader (MonadTrans (lift), ReaderT (runReaderT), asks)
+import Control.Monad.State (MonadState (get), State, modify, runState)
 
 type Inventory = [(String, Int)]
 
 decreaseStock :: String -> ExceptT String (State Inventory) Int
-decreaseStock = undefined
+decreaseStock fruit = do
+  inv <- get
+  case lookup fruit inv of
+    Just 0 -> throwError "out of stock"
+    Just stock -> do
+      modify $ map dec
+      pure $ stock - 1
+    Nothing -> throwError "not found"
+  where
+    dec (k, v)
+      | k == fruit = (k, v - 1)
+      | otherwise = (k, v)
 
-runStock
-  :: ExceptT String (State Inventory) a
-  -> Inventory
-  -> (Either String a, Inventory)
-runStock = undefined
+runStock ::
+  ExceptT String (State Inventory) a ->
+  Inventory ->
+  (Either String a, Inventory)
+runStock m = runState $ runExceptT m
 
 data AppConfig = AppConfig
-  { prefix :: String
-  , suffix :: String
+  { prefix :: String,
+    suffix :: String
   }
   deriving (Eq, Show)
 
 decorate :: String -> ReaderT AppConfig (Either String) String
-decorate = undefined
+decorate "" = lift $ Left "empty"
+decorate s = do
+  pref <- asks prefix
+  suff <- asks suffix
+  pure $ pref ++ s ++ suff
 
 runDecorate :: String -> AppConfig -> Either String String
-runDecorate = undefined
+runDecorate s cfg = runReaderT (decorate s) cfg
