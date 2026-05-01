0b01ac53803c93384e6a77b15e08f9863177eca7
tmtmtoo <tmtmt2209@gmail.com>
2026-05-01 04:03:22 +0000

    chore: lesson 007 monad

--- 007-monad/src/Lesson.hs
+++ 007-monad/src/Lesson.hs
@@ -1,29 +1,35 @@
 module Lesson
-  ( Identity' (..)
-  , safeDiv
-  , chainDiv
-  , Expr (..)
-  , evalExpr
-  ) where
+  ( Identity' (..),
+    safeDiv,
+    chainDiv,
+    Expr (..),
+    evalExpr,
+  )
+where
 
-newtype Identity' a = Identity' { runIdentity' :: a }
+newtype Identity' a = Identity' {runIdentity' :: a}
   deriving (Eq, Show)
 
 instance Functor Identity' where
-  fmap = undefined
+  fmap f (Identity' a) = Identity' $ f a
 
 instance Applicative Identity' where
-  pure  = undefined
-  (<*>) = undefined
+  pure = Identity'
+  Identity' f <*> Identity' a = Identity' $ f a
 
 instance Monad Identity' where
-  (>>=) = undefined
+  Identity' a >>= f = f a
 
 safeDiv :: Int -> Int -> Maybe Int
-safeDiv = undefined
+safeDiv _ 0 = Nothing
+safeDiv x y = Just $ x `div` y
 
 chainDiv :: Int -> [Int] -> Maybe Int
-chainDiv = undefined
+chainDiv a [] = Just a
+chainDiv a (x : xs) = do
+  b <- safeDiv a x
+  c <- chainDiv b xs
+  return c
 
 data Expr
   = ELit Int
@@ -32,4 +38,14 @@ data Expr
   deriving (Eq, Show)
 
 evalExpr :: Expr -> Either String Int
-evalExpr = undefined
+evalExpr (ELit a) = Right a
+evalExpr (EAdd l r) = do
+  l' <- evalExpr l
+  r' <- evalExpr r
+  return $ l' + r'
+evalExpr (EDiv l r) = do
+  l' <- evalExpr l
+  r' <- evalExpr r
+  case safeDiv l' r' of
+    Nothing -> Left "division by zero"
+    Just x -> Right x
